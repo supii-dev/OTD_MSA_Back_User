51233a20da1f8c95c515b3b0b54aa3807ccd3cce
d-kjh <dkjh9942@gmail.com>
2025-09-09 17:18:52 +0900

    test

--- src/main/java/com/otd/otd_challenge/application/challenge/model/ChallengeGetRes.java
+++ src/main/java/com/otd/otd_challenge/application/challenge/model/ChallengeGetRes.java
@@ -2,11 +2,9 @@ package com.otd.otd_challenge.application.challenge.model;
 
 import lombok.Getter;
 import lombok.Setter;
-import lombok.ToString;
 
 @Getter
 @Setter
-@ToString
 public class ChallengeGetRes {
     private Long id;
     private int goal;
