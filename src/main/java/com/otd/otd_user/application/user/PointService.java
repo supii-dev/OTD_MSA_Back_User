2206435166b1347a6947f7ea01b7396016b5f8d6
supii <hwangsubin93@gmail.com>
2025-10-07 12:36:30 +0900

    포인트리스트불러오기

--- src/main/java/com/otd/otd_user/application/user/PointService.java
+++ src/main/java/com/otd/otd_user/application/user/PointService.java
@@ -1,14 +1,25 @@
 package com.otd.otd_user.application.user;
 
+import com.otd.configuration.enumcode.model.EnumChallengeRole;
+import com.otd.otd_challenge.application.challenge.ChallengeMapper;
+import com.otd.otd_challenge.application.challenge.Repository.ChallengeDefinitionRepository;
+import com.otd.otd_challenge.application.challenge.model.detail.ChallengeProgressGetReq;
+import com.otd.otd_challenge.application.challenge.model.detail.ChallengeProgressGetRes;
+import com.otd.otd_challenge.application.challenge.model.home.ChallengeHomeGetRes;
+import com.otd.otd_challenge.application.challenge.model.home.ChallengeMissionCompleteGetRes;
+import com.otd.otd_challenge.application.challenge.model.home.UserInfoGetRes;
+import com.otd.otd_challenge.entity.ChallengeDefinition;
 import com.otd.otd_challenge.entity.ChallengePointHistory;
 import com.otd.otd_user.application.user.model.PointHistoryDTO;
 import com.otd.otd_user.application.user.model.PointHistoryResponseDTO;
 import com.otd.otd_user.entity.User;
 import lombok.RequiredArgsConstructor;
+import lombok.Value;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -20,6 +31,9 @@ public class PointService {
 
     private final UserChallengePointRepository userChallengePointRepository;
     private final UserRepository userRepository;
+    private final ChallengeMapper challengeMapper;
+    private final ChallengeDefinitionRepository challengeDefinitionRepository;
+    private final UserMapper userMapper;
 
     public PointHistoryResponseDTO getPointHistory(Long userId) {
         log.info("===== 포인트 내역 조회 시작 =====");
@@ -68,4 +82,53 @@ public class PointService {
                 .pointHistory(historyDTOs)
                 .build();
     }
+    //전체 조회
+    public ChallengeHomeGetRes getSelectedListAll(Long userId, ChallengeProgressGetReq req) {
+        req.setUserId(userId);
+        List<ChallengeProgressGetRes> monthly = challengeMapper.findAllMonthlyFromUserId(req);
+        List<ChallengeProgressGetRes> weekly = challengeMapper.findAllWeeklyFromUserId(req);
+        List<ChallengeDefinition> daily = challengeDefinitionRepository.findByCdType("daily");
+        User userInfo = userRepository.findByUserId(userId);
+        Integer success = challengeMapper.findSuccessChallenge(userId);
+
+        //전체 미션 완료 내역 조회
+        List<ChallengeMissionCompleteGetRes> missionComplete =
+                userMapper.findAllMissionCompleteByUserId(userId);
+
+        List<ChallengeProgressGetRes> personal = new ArrayList<>();
+        List<ChallengeProgressGetRes> competition = new ArrayList<>();
+        int successCount = (success != null) ? success : 0;
+
+        EnumChallengeRole cr = userInfo.getChallengeRole();
+
+        UserInfoGetRes uInfo = UserInfoGetRes.builder()
+                .userId(userId)
+                .name(userInfo.getName())
+                .nickName(userInfo.getNickName())
+                .pic(userInfo.getPic())
+                .xp(userInfo.getXp())
+                .challengeRole(cr)
+                .point(userInfo.getPoint()).build();
+
+
+
+        for (ChallengeProgressGetRes challengeProgressGetRes : monthly) {
+            switch (challengeProgressGetRes.getType()) {
+                case "personal" -> personal.add(challengeProgressGetRes);
+                case "competition" -> competition.add(challengeProgressGetRes);
+            }
+        }
+
+        return ChallengeHomeGetRes.builder()
+                .user(uInfo)
+                .success(successCount)
+                .dailyMission(daily)
+                .weeklyChallenge(weekly)
+                .competitionChallenge(competition)
+                .personalChallenge(personal)
+                .missionComplete(missionComplete)
+                .build();
+    }
 }
+
+
--- src/main/java/com/otd/otd_user/application/user/UserMapper.java
+++ src/main/java/com/otd/otd_user/application/user/UserMapper.java
@@ -1,9 +1,12 @@
 package com.otd.otd_user.application.user;
 
+import com.otd.otd_challenge.application.challenge.model.home.ChallengeMissionCompleteGetRes;
 import com.otd.otd_user.application.user.model.UserLoginRes;
 import com.otd.otd_user.application.user.model.UserProfileGetDto;
 import com.otd.otd_user.application.user.model.UserProfileGetRes;
 import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
 
 @Mapper
 public interface UserMapper {
@@ -11,5 +14,6 @@ public interface UserMapper {
     int countByUid(String uid);
     int countByNickname(String nickname);
     UserLoginRes findRoleByUserId(long signedUserId);
+    List<ChallengeMissionCompleteGetRes> findAllMissionCompleteByUserId(Long userId);
 
 }
