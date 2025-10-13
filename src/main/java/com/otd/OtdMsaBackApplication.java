40e75c139a5939a62ef13e5f78ddb9c31f109401
jinseungbok <92.bokjs@gmail.com>
2025-10-13 11:54:09 +0900

    "[docs]:251013_data debug 1"

--- src/main/java/com/otd/OtdMsaBackApplication.java
+++ src/main/java/com/otd/OtdMsaBackApplication.java
@@ -11,9 +11,26 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @ConfigurationPropertiesScan
 @SpringBootApplication
 @EnableFeignClients(basePackages = "com.otd.configuration.feignclient")
-@ComponentScan(basePackages = {"com.otd","com.otd.configuration", "com.otd.otd_user", "com.otd.otd_challenge", "com.otd.otd_pointShop"})
-@EntityScan(basePackages = {"com.otd.otd_user.entity", "com.otd.otd_challenge.entity", "com.otd.otd_pointShop.entity"})
-@EnableJpaRepositories(basePackages = {"com.otd.otd_pointShop.repository", "com.otd.otd_user.application.user"})
+@ComponentScan(basePackages = {
+        "com.otd",
+        "com.otd.configuration",
+        "com.otd.otd_user",
+        "com.otd.otd_challenge",
+        "com.otd.otd_pointShop",
+        "com.otd.otd_admin"
+})
+@EntityScan(basePackages = {
+        "com.otd.otd_user.entity",
+        "com.otd.otd_challenge.entity",
+        "com.otd.otd_pointShop.entity",
+        "com.otd.otd_admin.entity"
+})
+@EnableJpaRepositories(basePackages = {
+        "com.otd.otd_user.application",
+        "com.otd.otd_challenge.application.challenge.Repository",
+        "com.otd.otd_pointShop.repository",
+        "com.otd.otd_admin.application.admin.Repository"
+})
 public class OtdMsaBackApplication {
     public static void main(String[] args) {
         SpringApplication.run(OtdMsaBackApplication.class, args);
