edf986afd78dcf0516e62368d5138198a7920459
d-kjh <dkjh9942@gmail.com>
2025-10-15 17:05:29 +0900

    10/15 feign 수정
    # Conflicts:
    #	src/main/java/com/otd/otd_challenge/application/challenge/ChallengeService.java

--- src/main/java/com/otd/otd_challenge/application/challenge/ChallengeService.java
+++ src/main/java/com/otd/otd_challenge/application/challenge/ChallengeService.java
@@ -230,7 +230,7 @@ public class ChallengeService {
         return map;
     }
 
-    @Transactional
+   // @Transactional
     public ResultResponse<?> saveMissionRecord(Long userId, ChallengeRecordMissionPostReq req){
         int result = challengeMapper.saveMissionRecordByUserIdAndCpId(userId, req.getCdId());
         ChallengeDefinition point = challengeDefinitionRepository.findByCdId(req.getCdId());
